adb3dc0e5d9e5186d594135530420dae0960ef61
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-04-06 17:33:07 +0100

    Edits

--- fonts/hack_install.sh
+++ fonts/hack_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# Install hack font
+# Get hack font
 
 wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
--- fonts/monaco_improved_install.sh
+++ fonts/monaco_improved_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# Install better monaco font
+# Get better monaco font
 
 wget https://github.com/probil/Monaco-IDE-font/raw/master/dist/Monaco.ttf
