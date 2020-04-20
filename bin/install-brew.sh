a4a50d81fac4acc5811920a9aa6b85ea2c3d69fd
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-04-20 12:50:16 +0100

    Quick commit

--- bin/install-brew.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# dotfiles setup script - install brew packages
-
-brew ls --versions $1 >/dev/null && {
-  printf "[.] $1 already installed.\n"
-} || {
-  printf "[+] installing $1\n"
-  brew install $1
-}
