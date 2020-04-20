27112ae934ff4a271bb5d9bcc8ba99797f4bc2ec
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-04-20 12:49:26 +0100

    Quick commit

---
+++ bin/dots_setup/install-brew.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# dotfiles setup script - install brew packages
+
+brew ls --versions $1 >/dev/null && {
+  printf "[.] $1 already installed.\n"
+} || {
+  printf "[+] installing $1\n"
+  brew install $1
+}
---
+++ bin/dots_setup/install-cask.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# dotfiles setup script - install casks
+
+brew ls --versions $1 >/dev/null && {
+  printf "[.] cask $1 already installed via Homebrew\n"
+} || {
+  _name=$(
+    echo $1 |
+      sed -E 's/[-_]+/ /g'
+  )
+  ls /Applications | grep -ri "$_name" >/dev/null && {
+    printf "[.] $1 seems already installed in /Applications\n"
+  } || {
+    printf "[+] installing $1\n"
+    brew cask install $1
+  }
+}
