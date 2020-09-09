1da4be3c0142e241217ae020e38f422396e1a3e9
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-09-09 23:51:51 +0100

    Quick commit

--- zsh/.zsh/settings.zsh
+++ zsh/.zsh/settings.zsh
@@ -53,10 +53,10 @@ autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey '^[[A'  up-line-or-beginning-search    # Arrow up
-bindkey '^[OA'  up-line-or-beginning-search
-bindkey '^[[B'  down-line-or-beginning-search  # Arrow down
-bindkey '^[OB'  down-line-or-beginning-search
+bindkey '^[[A' up-line-or-beginning-search    # Arrow up
+bindkey '^[OA' up-line-or-beginning-search
+bindkey '^[[B' down-line-or-beginning-search  # Arrow down
+bindkey '^[OB' down-line-or-beginning-search
 
 # Activate vim-mode
 # -----------------
