266336af87151a5daf0c1955b03037a5bfd49155
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-10-29 18:05:28 +0000

    Default emacs font

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -38,7 +38,7 @@
 
 ;; Font settings
 (setq
- doom-font (font-spec :family "Sometype Mono" :size 13)
+ doom-font (font-spec :family "Anonymous Pro" :size 14)
  doom-variable-pitch-font (font-spec :family "Verdana" :size 12)
  doom-big-font-increment 1)
 
