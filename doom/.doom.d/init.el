b20f7e55cf97c44e1b31d3c70e80f4290f0d3914
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-12-11 23:53:07 +0000

    Activate makefile-executor

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -86,7 +86,7 @@
  lsp
  macos             ; MacOS-specific commands
  magit             ; a git porcelain for Emacs
- ;;make              ; run make tasks from Emacs
+ make              ; run make tasks from Emacs
  ;;pass              ; password manager for nerds
  ;;pdf               ; pdf enhancements
  ;;prodigy           ; FIXME managing external services & code builders
