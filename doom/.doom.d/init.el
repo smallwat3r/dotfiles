a900d3587aeda9a35780bf148d802f810ebb44f5
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-12-14 23:08:32 +0000

    Updates

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -22,7 +22,7 @@
  ;;indent-guides    ; highlighted indent columns
  ;;minimap           ; show a map of the code on the side
  ;;modeline          ; snazzy, Atom-inspired modeline, plus API
- ;; nav-flash         ; blink cursor line after big motions
+ ;;nav-flash         ; blink cursor line after big motions
  ;;neotree           ; a project drawer, like NERDTree for vim
  ophints           ; highlight the region an operation acts on
  (popup
@@ -32,7 +32,7 @@
  treemacs          ; a project drawer, like neotree but cooler
  ;;unicode           ; extended unicode support for various languages
  vc-gutter         ; vcs diff in the fringe
- vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+ ;;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
  ;;window-select     ; visually switch windows
  workspaces        ; tab emulation, persistence & separate workspaces
  zen               ; distraction-free coding or writing
