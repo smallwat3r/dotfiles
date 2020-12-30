1bee19f9dd602269c0951936847003eb6e4d6377
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-12-30 11:02:47 +0100

    Update bindings

--- doom/.doom.d/+bindings.el
+++ doom/.doom.d/+bindings.el
@@ -1,9 +1,12 @@
 ;;; $DOOMDIR/+bindings.el -*- lexical-binding: t; -*-
 
 (map!
+
+ ;; Defaults
  "C-j"       #'scroll-up-line  ; scroll
  "C-k"       #'scroll-down-line
 
+ ;; Override
  (:map override
   "S-C-h"    #'shrink-window-horizontally  ; window sizes
   "S-C-l"    #'enlarge-window-horizontally
@@ -15,9 +18,13 @@
   "M-k"      #'windmove-up
   "M-j"      #'windmove-down
 
+  "M-SPC"    #'cycle-spacing
+  "M-o"      #'delete-blank-lines
+
   "M-n"      #'+default/new-buffer
   "M-`"      #'other-frame)
 
+ ;; Normal mode
  (:map evil-normal-state-map
   ";f"       #'format-all-buffer
   ";w"       #'evil-write
@@ -32,7 +39,6 @@
  ;; Markdown
  (:leader
   (:map markdown-mode-map
-
    (:prefix ("I" . "md-insert")
     :desc "Blockquote"    "q" #'markdown-insert-blockquote
     :desc "Bold"          "b" #'markdown-insert-bold
@@ -53,30 +59,35 @@
     :desc "Five"  "5" #'markdown-insert-header-atx-5
     :desc "Six"   "6" #'markdown-insert-header-atx-6)))
 
+ ;; localleader
  (:leader
-  (:prefix "m" ; localleader
+  (:prefix "m"
    :desc "Makefile run"             "r" #'+make/run))
 
+ ;; Buffers
  (:leader
-  (:prefix "b"  ; buffers
+  (:prefix "b"
    :desc "Kill buffer"              "d" #'evil-delete-buffer))
 
+ ;; Open
  (:leader
-  (:prefix "o"  ; open
+  (:prefix "o"
    :desc "Emails"                   "m" #'notmuch
    :desc "Reveal in Finder"         "o" #'+macos/reveal-in-finder
    :desc "Reveal project in Finder" "O" #'+macos/reveal-project-in-finder))
 
+ ;; Errors
  (:leader
-  (:prefix ("e" . "errors")  ; erros
+  (:prefix ("e" . "errors")
    :desc "Flycheck list errors"     "l" #'flycheck-list-errors
    :desc "Flycheck next error"      "n" #'flycheck-next-error
    :desc "Flycheck previous error"  "p" #'flycheck-previous-error
    :desc "Flycheck explain error"   "e" #'flycheck-explain-error-at-point
    :desc "Flycheck verify setup"    "v" #'flycheck-verify-setup))
 
+ ;; Kubernetes
  (:leader
-  (:prefix ("k" . "kubernetes")  ; kubernetes
+  (:prefix ("k" . "kubernetes")
    :desc "Overview"                 "o" #'kubernetes-overview
    :desc "Set namespace"            "n" #'kubernetes-set-namespace
    :desc "Display service"          "s" #'kubernetes-display-service
@@ -84,26 +95,31 @@
    :desc "Describe"                 "D" #'kubernetes-describe-pod
    :desc "Exec into"                "e" #'kubernetes-exec-into))
 
+ ;; Docker
  (:leader
-  (:prefix ("d" . "docker")  ; docker
+  (:prefix ("d" . "docker")
    :desc "Images"                   "i" #'docker-images
    :desc "Exec into"                "e" #'docker-container-shell))
 
+ ;; Search
  (:leader
-  (:prefix "s"  ; search
+  (:prefix "s"
    :desc "Ripgrep"                  "g" #'ripgrep-regexp))
 
+ ;; Notes
  (:leader
-  (:prefix "n"  ; notes
+  (:prefix "n"
    :desc "Deft open"                "D" #'deft
    :desc "Deft new"                 "d" #'deft-new-file))
 
+ ;; Org-journal
  (:after org-journal
   :leader
   (:prefix ("j" . "journal")
    :desc "Search journal"           "s" #'org-journal-search
    :desc "New journal entry"        "n" #'org-journal-new-entry))
 
+ ;; Python
  (:after python
   :leader
   :map python-mode-map
