7b0b6e955e4efad73dd361439d7aa15c4b37fd6b
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-02-11 22:05:43 +0000

    Update list of abbreviations

--- doom/.doom.d/abbrev.el
+++ doom/.doom.d/abbrev.el
@@ -1,13 +1,20 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
-;; Global list of abbreviations
+;; Manage abbrev
+
+(clear-abbrev-table global-abbrev-table)
+
 (define-abbrev-table 'global-abbrev-table
   '(
+    ;; General
     ("btw"  "by the way")
     ("pov"  "point of view")
     ("atm"  "at the moment" )
     ("plz"  "please")
+    ("ty"   "thank you")
+    ("afaict" "as far as I can tell")
 
+    ;; Typos
     ("altough"  "although")
     ("widht"    "width")
     ("thougth"  "thought")
@@ -17,7 +24,59 @@
     ("weigth"   "weight")
     ("wether"   "whether")
 
+    ;; Unicode stuff
+    ("uok" "✗")
+    ("uno" "✓")
+    ("ura" "→" )
+
+    ;; Personal stuff
     ("zma" "Matthieu Petiteau")
     ("zme" "mpetiteau.pro@gmail.com")
     ("zco" "Copyright 2020 Matthieu Petiteau, all rights reserved.")
     ))
+
+;;
+;; Define abbreviations from specific major-mode
+;;
+
+(define-abbrev-table 'prog-mode-abbrev-table
+  '(
+    ("rt" "return")
+    ("ud" "undefined")
+    ))
+
+(define-abbrev-table 'sh-mode-abbrev-table
+  '(
+    ("#sh" "#!/usr/bin/env bash")
+    ("ec" "echo")
+    ("pf" "printf")
+    ))
+
+(define-abbrev-table 'python-mode-abbrev-table
+  '(
+    ("#sh" "#!/usr/bin/env python")
+    ("ffi" "from flask import")
+    ("pr" "print")
+    ("cl" "class")
+    ("ifn" "if __name__ == \"__main__\":\n")
+    ("pdb" "import pdb; pdb.set_trace()  # Debug")
+    ))
+
+(define-abbrev-table 'sql-mode-abbrev-table
+  '(
+    ("s" "SELECT")
+    ("f" "FROM")
+    ("w" "WHERE")
+    ("d" "DISTINCT")
+    ("ob" "ORDER BY")
+    ("gb" "GROUP BY")
+    ("l" "LIMIT")
+    ("u" "UNION")
+    ("j" "JOIN")
+    ("lj" "LEFT JOIN")
+    ("rj" "RIGHT JOIN")
+    ))
+
+(set-default 'abbrev-mode t)
+
+(setq save-abbrevs nil)
