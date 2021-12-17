44736937f4e2df3f24cb8bf6378d482c483d4748
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-17 21:54:34 +0000

    Do not emulate vim bindings if in emacs

--- zsh/.zsh/utils/2-vim-emulation.zsh
+++ zsh/.zsh/utils/2-vim-emulation.zsh
@@ -1,19 +1,22 @@
-setopt VI  # Emulate vim mode in zsh (same as 'bindkey -v')
+# vim-bindings emulation, only if outside of Emacs
+if [[ ! "$INSIDE_EMACS" ]]; then
+  setopt VI  # same as 'bindkey -v'
 
-export KEYTIMEOUT=20  # need at least 20 for jk binding to work properly
-bindkey -M viins 'jk' vi-cmd-mode
+  export KEYTIMEOUT=20  # need at least 20 for jk binding to work properly
+  bindkey -M viins 'jk' vi-cmd-mode
 
-bindkey '^?' backward-delete-char
+  bindkey '^?' backward-delete-char
 
-# yank to clipboard
-__vi_yank_pbcopy() {
-  zle vi-yank
-  echo "$CUTBUFFER" | pbcopy
-}
-zle -N __vi_yank_pbcopy
-bindkey -M vicmd 'y' __vi_yank_pbcopy
+  # yank to clipboard
+  __vi_yank_pbcopy() {
+    zle vi-yank
+    echo "$CUTBUFFER" | pbcopy
+  }
+  zle -N __vi_yank_pbcopy
+  bindkey -M vicmd 'y' __vi_yank_pbcopy
 
-# Edit command in preferred editor
-autoload edit-command-line
-zle -N edit-command-line
-bindkey '^e' edit-command-line
+  # Edit command in preferred editor
+  autoload edit-command-line
+  zle -N edit-command-line
+  bindkey '^e' edit-command-line
+fi
