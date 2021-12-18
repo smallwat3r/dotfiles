70de582bafaca79686acf08c631ae2674998aab9
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-18 00:59:26 +0000

    Avoid recursion from function name

--- zsh/.zsh/extensions/nvm.zsh
+++ zsh/.zsh/extensions/nvm.zsh
@@ -2,7 +2,7 @@ export NVM_DIR="$HOME/.nvm"
 
 # I find nvm quite bulky to load on startup, so this provides a way to lazy load it
 nvm() {
-  if ! command -v foo >/dev/null; then
+  if ! command -v nvm --help >/dev/null; then
     [ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"
     [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
   fi
