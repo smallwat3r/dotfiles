f9f9c0fa6002e14457f5c2bb724d01e3b92f3b86
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-18 00:53:12 +0000

    Lazy load nvm

--- zsh/.zsh/extensions/nvm.zsh
+++ zsh/.zsh/extensions/nvm.zsh
@@ -1,8 +1,11 @@
 export NVM_DIR="$HOME/.nvm"
 
-# I find nvm quite bulky in my zsh config, so this function provides a way to manually lazy load nvm
-# when I need to use it.
-load-nvm() {
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+# I find nvm quite bulky in my zsh config, so this provides a way to lazy load it
+nvm() {
+  if ! command -v foo >/dev/null; then
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"
+    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+  fi
+
+  nvm "$@"
 }
