b0f672923afff870fc949f6016a2c2bffc087e01
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-20 21:16:43 +0000

    Adding history search

--- zsh/.zsh/utils/1-completion.zsh
+++ zsh/.zsh/utils/1-completion.zsh
@@ -1,4 +1,5 @@
 autoload -U compinit && compinit
+
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' use-cache on
@@ -8,6 +9,7 @@ zstyle ':completion:*' verbose yes
 zstyle ':completion:*' file-sort modification
 
 zmodload zsh/complist
+
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'j' vi-down-line-or-history
