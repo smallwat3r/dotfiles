59eb82ddf8af9178dd9a6d9ecd2d2691ecaa51de
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-21 23:31:40 +0000

    Update zsh prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,39 +1,3 @@
-__git_root() {
-  if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]]; then
-    [[ $(git rev-parse --show-toplevel 2>/dev/null) == "$PWD" ]] && echo true
-  fi
-}
-
-__git_dirty() {
-  [[ $(git diff --shortstat 2>/dev/null | tail -n1) != "" ]] && echo "*"
-}
-
-__git_branch() {
-  git branch --no-color 2>/dev/null \
-    | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(__git_dirty)/"
-}
-
-__display_git_info() {
-  local _git_root="$(__git_root | sed 's/true/~/')"
-  local _git_branch="$(__git_branch)"
-  [[ -n $_git_branch ]] && echo " ${_git_branch}${_git_root}"
-}
-
-__shrink_path() {
-  echo ~+ \
-    | sed "s;$HOME;~;" \
-    | sed 's;\(/.\)[^/]*;\1;g' \
-    | sed 's/.$//'
-}
-
-__path() {
-  case $PWD in
-    "$HOME") printf '~' ;;
-    "/") printf '/' ;;
-    *) printf '%s%s' "$(__shrink_path)" "${PWD##*/}" ;;
-  esac
-}
-
 __is_venv() {
   if [[ $VIRTUAL_ENV ]]; then
     echo '%s' "(.${VIRTUAL_ENV##*/}) "
@@ -43,16 +7,29 @@ __is_venv() {
 VIRTUAL_ENV_DISABLE_PROMPT=false
 setopt PROMPT_SUBST
 
-# If we are from Emacs, run a standard prompt and do not run tmux. Else, in other shell emulators
-# activate tmux by default and use the individual pane titles to display the main prompt information.
-if [[ "$INSIDE_EMACS" ]]; then
-  PROMPT='%(?..%? )$(__is_venv)$(__path)$(__display_git_info) %# '
-else
+autoload -U colors && colors
+autoload -Uz vcs_info
+
+precmd_vcs_info() {
+  vcs_info
+}
+precmd_functions+=(
+  precmd_vcs_info
+)
+
+RPROMPT=\$vcs_info_msg_0_
+
+zstyle ':vcs_info:git:*' formats '%F{245}(%b)%r%f'
+zstyle ':vcs_info:*' enable git
+
+PROMPT='%(?..%F{red}?%? )%f$(__is_venv)%F{cyan}%2~%f %# '
+
+# When outside of emacs, activate tmux by default and use the individual pane titles
+# to display the main prompt information.
+if [[ ! "$INSIDE_EMACS" ]]; then
   if [ -t 0 ] && [[ -z $TMUX ]] && [[ $- = *i* ]]; then
     exec tmux
   fi
-
-  PROMPT='%(?..%? )$(__is_venv)%# '
 
   __pane_number() {
     tmux list-panes | grep "active" | cut -d ':' -f 1
@@ -64,6 +41,42 @@ else
         -t "$(__pane_number)" \
         -T "#[fg=red,bold]$(echo "$*" | cut -d . -f 1)#[fg=default]"
     command ssh "$@"
+  }
+
+  __git_dirty() {
+    [[ $(git diff --shortstat 2>/dev/null | tail -n1) != "" ]] && echo "*"
+  }
+
+  __git_root() {
+    if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]]; then
+      [[ $(git rev-parse --show-toplevel 2>/dev/null) == "$PWD" ]] && echo true
+    fi
+  }
+
+  __git_branch() {
+    git branch --no-color 2>/dev/null \
+      | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(__git_dirty)/"
+  }
+
+  __display_git_info() {
+    local _git_root="$(__git_root | sed 's/true/~/')"
+    local _git_branch="$(__git_branch)"
+    [[ -n $_git_branch ]] && echo " ${_git_branch}${_git_root}"
+  }
+
+  __shrink_path() {
+    echo ~+ \
+      | sed "s;$HOME;~;" \
+      | sed 's;\(/.\)[^/]*;\1;g' \
+      | sed 's/.$//'
+  }
+
+  __path() {
+    case $PWD in
+      "$HOME") printf '~' ;;
+      "/") printf '/' ;;
+      *) printf '%s%s' "$(__shrink_path)" "${PWD##*/}" ;;
+    esac
   }
 
   precmd() {
