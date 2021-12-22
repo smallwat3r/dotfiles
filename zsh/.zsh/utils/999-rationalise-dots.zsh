1b62775725335c946b35863647ef9e7819ac5daa
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-22 16:47:15 +0000

    Lint

--- zsh/.zsh/utils/999-rationalise-dots.zsh
+++ zsh/.zsh/utils/999-rationalise-dots.zsh
@@ -1,6 +1,8 @@
 # Move up directories i.e. ... >> ../.. and .... >> ../../... etc.
+
 __rationalise-dot() {
   [[ $LBUFFER = *.. ]] && LBUFFER+=/.. || LBUFFER+=.
 }
+
 zle -N __rationalise-dot
 bindkey "." __rationalise-dot
