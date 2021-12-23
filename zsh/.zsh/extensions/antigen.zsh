3cbdaa58a68a180846e6855a426669f2bbc20840
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-23 18:45:59 +0100

    Updates

--- zsh/.zsh/extensions/antigen.zsh
+++ zsh/.zsh/extensions/antigen.zsh
@@ -1,8 +1,6 @@
 if [ -f /usr/local/share/antigen/antigen.zsh ]; then
   source '/usr/local/share/antigen/antigen.zsh'
-fi
 
-if ! command -v antigen >/dev/null; then
   antigen bundle zsh-users/zsh-autosuggestions
   antigen bundle zsh-users/zsh-syntax-highlighting
 
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -33,6 +33,6 @@ export TERM='xterm-256color'
 export CLICOLOR=1
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='0;31'
-export HISTFILE="$HOME/.zsh_history"
+export HISTFILE="${HOME}/.zsh_history"
 export HISTSIZE=999999999
 export SAVEHIST="${HISTSIZE}"
