4c7ae1c22b12bc781c5003db4be40f2f83b37c7c
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-23 19:08:00 +0100

    Define ZSH highlight env

--- zsh/.zsh/extensions/antigen.zsh
+++ zsh/.zsh/extensions/antigen.zsh
@@ -5,4 +5,19 @@ if [ -f /usr/local/share/antigen/antigen.zsh ]; then
   antigen bundle zsh-users/zsh-syntax-highlighting
 
   antigen apply
+
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+
+  ZSH_HIGHLIGHT_PATTERNS+=('rm -rf' 'fg=231,bg=red')
+  ZSH_HIGHLIGHT_PATTERNS+=('sudo' 'fg=231,bg=red')
+
+  ZSH_HIGHLIGHT_STYLES[bracket-error]='fg=231,bg=red'
+  ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=51'
+  ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=165'
+  ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=148'
+  ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=215'
+
+  ZSH_HIGHLIGHT_STYLES[alias]='fg=51'
+  ZSH_HIGHLIGHT_STYLES[global-alias]='fg=51'
+  ZSH_HIGHLIGHT_STYLES[globbing]='fg=165'
 fi
