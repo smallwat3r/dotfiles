613aa52cd294bf15cbc3f3b6aa4a5745a350942f
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-12-24 18:04:09 +0100

    Source vterm handler functions

--- zsh/.zsh/extensions/20-emacs.zsh
+++ zsh/.zsh/extensions/20-emacs.zsh
@@ -4,3 +4,7 @@ export EMACS_DOOM="$HOME/.emacs.doom"
 if [[ ! "${PATH}" == "*/${EMACS_DOOM}/bin*" ]]; then
   export PATH="${PATH:+${PATH}:}${EMACS_DOOM}/bin"
 fi
+
+if [[ "$INSIDE_EMACS" = 'vterm' ]] && [[ -n "${EMACS_VTERM_PATH}" ]] && [[ -f "${EMACS_VTERM_PATH}"/etc/emacs-vterm-zsh.sh ]]; then
+  source "${EMACS_VTERM_PATH}"/etc/emacs-vterm-zsh.sh
+fi
