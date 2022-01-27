380973f69dcc4e911696edeefe30390785c2c147
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-01-27 20:35:26 +0000

    Surround variables

--- zsh/.zsh/extensions/20-emacs.zsh
+++ zsh/.zsh/extensions/20-emacs.zsh
@@ -1,11 +1,11 @@
 export EMACS='/usr/local/bin/emacs'
-export EMACS_DOOM="$HOME/.emacs.doom"
+export EMACS_DOOM="${HOME}/.emacs.doom"
 
 if [[ ! "${PATH}" == "*/${EMACS_DOOM}/bin*" ]]; then
   export PATH="${PATH:+${PATH}:}${EMACS_DOOM}/bin"
 fi
 
-if [[ "$INSIDE_EMACS" = 'vterm' ]] \
+if [[ "${INSIDE_EMACS}" = 'vterm' ]] \
   && [[ -n "${EMACS_VTERM_PATH}" ]] \
   && [[ -f "${EMACS_VTERM_PATH}"/etc/emacs-vterm-zsh.sh ]]; then
   source "${EMACS_VTERM_PATH}"/etc/emacs-vterm-zsh.sh
