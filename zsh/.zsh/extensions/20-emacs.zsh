bb6890d47d1171787fc55bdc583ce70373f05d0f
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-01-27 19:54:46 +0000

    Add emacs utility function

--- zsh/.zsh/extensions/20-emacs.zsh
+++ zsh/.zsh/extensions/20-emacs.zsh
@@ -5,6 +5,15 @@ if [[ ! "${PATH}" == "*/${EMACS_DOOM}/bin*" ]]; then
   export PATH="${PATH:+${PATH}:}${EMACS_DOOM}/bin"
 fi
 
-if [[ "$INSIDE_EMACS" = 'vterm' ]] && [[ -n "${EMACS_VTERM_PATH}" ]] && [[ -f "${EMACS_VTERM_PATH}"/etc/emacs-vterm-zsh.sh ]]; then
+if [[ "$INSIDE_EMACS" = 'vterm' ]] \
+  && [[ -n "${EMACS_VTERM_PATH}" ]] \
+  && [[ -f "${EMACS_VTERM_PATH}"/etc/emacs-vterm-zsh.sh ]]; then
   source "${EMACS_VTERM_PATH}"/etc/emacs-vterm-zsh.sh
 fi
+
+projectile-clean-cache() {
+  if [[ -f "${EMACS_DOOM}/.local/cache/projectile.cache" ]]; then
+    rm ${EMACS_DOOM}/.local/cache/projectile.cache
+    printf "Projectile cache cleaned.\n"
+  fi
+}
