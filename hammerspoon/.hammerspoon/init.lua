f4b3747b7b53ffc94dae38f956f125ed401695dd
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-02-05 23:11:13 +0000

    Typo

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -15,7 +15,7 @@ end
 -- Karabiner is mapping F18 to caps-lock, this works as our hyper key
 f18 = hs.hotkey.bind({}, 'F18', enterHyperMode, exitHyperMode)
 
--- Trigger emacs everywhere
+-- Trigger emacs-everywhere
 hyper:bind({}, "e", function()
   hs.task.new("/bin/bash", nil, { "-l", "-c", "emacsclient --eval '(emacs-everywhere)'" }):start()
   hyper.triggered = true
