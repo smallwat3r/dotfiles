46b4e1dcd9d5ecd97f5d2ec5661e519dd4f25bc0
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-02-09 17:10:49 +0000

    Deft mode updates

--- emacs/.doom.d/modules/smallwat3r/deft/config.el
+++ emacs/.doom.d/modules/smallwat3r/deft/config.el
@@ -6,14 +6,13 @@
   :commands (deft deft-new-file-named)
   :custom
   (deft-recursive t)
-  (deft-use-filename-as-title nil)
   (deft-use-filter-string-for-filename t)
-  (deft-auto-save-interval -1.0)  ; disable auto-save
+  (deft-auto-save-interval 0)  ; disable auto-save
+  (deft-extensions '("org" "md" "txt"))
   (deft-file-naming-rules
     '((noslash . "-")
       (nospace . "-")
       (case-fn . downcase)))
-  :init (setq deft-default-extension "org")
   :config
   (set-evil-initial-state! 'deft-mode 'insert)  ; start on insert mode for filtering
   (add-hook 'deft-mode-hook #'doom-mark-buffer-as-real-h))
