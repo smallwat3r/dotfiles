ee33574d1bc5f5159fec15a1ddc9aa5b7ee49bd6
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-02-22 20:08:03 +0000

    Add: improve zsh vim mode

--- zsh/.zsh/utils/10-vim-bindings.zsh
+++ zsh/.zsh/utils/10-vim-bindings.zsh
@@ -9,6 +9,10 @@ if [[ ! "${INSIDE_EMACS}" || "${INSIDE_EMACS}" = 'alacritty' ]]; then
   export KEYTIMEOUT=20
   bindkey -M viins 'jk' vi-cmd-mode
 
+  # Fix deletion after switching modes
+  bindkey '^?' backward-delete-char
+
+  # Copy behaviour
   __vi_yank_pbcopy() {
     zle vi-yank
     echo "${CUTBUFFER}" | pbcopy
@@ -16,4 +20,27 @@ if [[ ! "${INSIDE_EMACS}" || "${INSIDE_EMACS}" = 'alacritty' ]]; then
 
   zle -N __vi_yank_pbcopy
   bindkey -M vicmd 'y' __vi_yank_pbcopy
+
+  # Manage cursor shape from insert and normal modes
+  function zle-keymap-select {
+    if [[ "${KEYMAP}" == vicmd ]] \
+      || [[ $1 = 'block' ]]; then
+      echo -ne '\e[1 q'
+
+    elif [[ "${KEYMAP}" == main ]] \
+      || [[ "${KEYMAP}" == viins ]] \
+      || [[ "${KEYMAP}" = '' ]] \
+      || [[ "${1}" = 'beam' ]]; then
+      echo -ne '\e[5 q'
+    fi
+  }
+  zle -N zle-keymap-select
+
+  echo -ne '\e[5 q'  # default cursor
+
+  __fix_cursor() {
+    echo -ne '\e[5 q'
+  }
+
+  precmd_functions+=(__fix_cursor)
 fi
