b7eeb1faa1e1c17f7fbebe667a36b7e3e5bc9e08
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-04-05 19:32:03 +0100

    Add: gpg cache functions

--- zsh/.zsh/extensions/20-gpg.zsh
+++ zsh/.zsh/extensions/20-gpg.zsh
@@ -1,5 +1,21 @@
 export GPG_TTY=$(tty)
 
+if [ -f /usr/local/bin/gpg-connect-agent ]; then
+  clear-cache-gpg-password() {
+    gpg-connect-agent reloadagent /bye
+  }
+fi
+
+if [ -f /usr/local/bin/gpg ]; then
+  gpg-pub-key() {
+    gpg --armor --export mpetiteau.pro@gmail.com
+  }
+
+  gpg-list-keys() {
+    gpg --list-secret-keys --keyid-format LONG
+  }
+fi
+
 if [ -f /usr/local/bin/keybase ]; then
   gpg-keybase-import() {
     keybase pgp export | gpg --import
