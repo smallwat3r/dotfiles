971fe587f92eddadfae46a33770128a7a27af2e9
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-06-06 22:20:46 +0100

    Add documentation

--- zsh/.zsh/extensions/20-tmux.zsh
+++ zsh/.zsh/extensions/20-tmux.zsh
@@ -1,20 +1,25 @@
 if [ -f /usr/local/bin/tmux ]; then
+  # Kill all Tmux sessions.
   tksa() {
     tmux kill-session -a
   }
 
+  # Kill current Tmux session.
   tks() {
     tmux kill-session -t
   }
 
+  # List all Tmux sessions.
   tls() {
     tmux list-sessions
   }
 
+  # Split current Tmux window horizontally.
   sp() {
     tmux splitw -v
   }
 
+  # Split current Tmux window vertically.
   vs() {
     tmux splitw -h
   }
