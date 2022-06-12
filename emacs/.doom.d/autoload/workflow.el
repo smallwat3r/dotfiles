f52a02edf232f952e890849ba2121752bd24ac5c
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-06-12 14:57:42 +0100

    Edit comment

--- emacs/.doom.d/autoload/workflow.el
+++ emacs/.doom.d/autoload/workflow.el
@@ -48,18 +48,20 @@
   "Kill current buffer."
   (interactive)
   (when (eq major-mode 'vterm-mode)
-    ;; If vterm is the major mode, disable confirmation for processes and
-    ;; buffer modified.
+    ;; If vterm is the major mode, disable confirmation for processes
+    ;; and buffer modified.
     (set-buffer-modified-p nil)
-    ;; Annoyingly every time I try to kill a vterm buffer it asks me for
-    ;; confirmation as it has a running process. This allows me to bypass
-    ;; this and kill it regardless.
-    ;; NOTE: Doom is supposed to have a hook for this, setting `confirm-kill-processes'
-    ;; to nil, but it doesn't seems to work properly for some reason.
+    ;; Annoyingly every time I try to kill a vterm buffer it asks me
+    ;; for confirmation as it has a running process. This allows me to
+    ;; bypass this and kill it regardless.
+    ;; NOTE: Doom is supposed to have a hook for this, setting
+    ;; `confirm-kill-processes' to nil, but it doesn't seems to work
+    ;; properly for some reason.
     (let ((proc (get-buffer-process (current-buffer))))
       (when (processp proc)
         (set-process-query-on-exit-flag proc nil))))
   (kill-this-buffer))
+
 
 ;;;###autoload
 (defun my/save-and-close-buffer ()
