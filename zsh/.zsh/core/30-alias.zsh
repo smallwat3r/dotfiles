c908db09dbf16aca4ee9a31b096a2f4963c444e3
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-06-13 17:51:17 +0100

    Better git alias management

--- zsh/.zsh/core/30-alias.zsh
+++ zsh/.zsh/core/30-alias.zsh
@@ -5,7 +5,7 @@ alias e="$EDITOR"
 
 # Handy to allow ignoring prompt characters when copying commands from
 # documentation. Also acts as an alias to make a command not appear in
-# the history when using HISTIGNORESPACE
+# the history when using HISTIGNORESPACE.
 alias \$=" "
 alias %=" "
 
@@ -18,7 +18,26 @@ alias lss="ls -la | grep --color=never '->'"
 alias mkdir="mkdir -pv"
 alias diskspace="df -P -kHl"
 
-alias g="git"
+# Define git aliases from git config.
+#
+# For all aliases set up in git config, define another alias such as all git
+# commands can be called using `g` directly with its alias concatenated.
+#
+# Examples:
+#   `git push` could be called from `gp`.
+#   `git checkout` could be called from `gco`.
+#
+__define_git_aliases() {
+  local aliases
+  aliases=(${^${${(0)"$(git config -z --get-regexp '^alias.')"}#alias.}/$'\n'/:alias for \'}\')
+  local al
+  for al in ${aliases%%:*}; do
+    alias g$al="git $al"
+  done
+  alias g="git"
+}
+__define_git_aliases
+
 alias python="python3"
 alias pip="pip3"
 
@@ -37,7 +56,7 @@ alias rg="rg \
 alias dots="cd $HOME/dotfiles"
 alias fonts="open $HOME/Library/Fonts"
 
-#global aliases
+# Global aliases
 alias -g G="| grep"
 alias -g L="| less"
 alias -g NE="2> /dev/null"
@@ -46,7 +65,7 @@ alias -g H="| head"
 alias -g T="| tail"
 alias -g S="| sort"
 
-# tracked aliases
+# Tracked aliases
 hash -d d="$HOME/dotfiles"
 hash -d g="$HOME/Github"
 hash -d dw="$HOME/Downloads"
