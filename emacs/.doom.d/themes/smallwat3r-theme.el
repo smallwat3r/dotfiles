9695880dbc11b0a44da7ea401b8be7dafac5a107
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-06-29 17:49:28 +0100

    Update faces

--- emacs/.doom.d/themes/smallwat3r-theme.el
+++ emacs/.doom.d/themes/smallwat3r-theme.el
@@ -16,10 +16,23 @@
  `(lazy-highlight ((t (:background "gold2" :foreground "black"))))
  `(whitespace-tab ((t (:background "PaleTurquoise1"))))
  `(fringe ((t (:background unspecified))))
-
  `(magit-section-highlight ((t (:background unspecified))))
  `(magit-diff-context-highlight ((t (:background unspecified))))
-
+ `(dired-subtree-depth-1-face ((t (:background unspecified))))
+ `(dired-subtree-depth-2-face ((t (:background unspecified))))
+ `(dired-subtree-depth-3-face ((t (:background unspecified))))
+ `(dired-subtree-depth-4-face ((t (:background unspecified))))
+ `(dired-subtree-depth-5-face ((t (:background unspecified))))
+ `(dired-subtree-depth-6-face ((t (:background unspecified))))
+ `(markdown-inline-code-face ((t (:box (:line-width 2 :style released-button)
+                                  :background "lightgrey"
+                                  :foreground "DarkMagenta"))))
+ `(markdown-pre-face ((t (:background "lightgrey" :foreground "black" :extend t))))
+ `(markdown-language-keyword-face ((t (:foreground "red3"))))
+ `(org-verbatim ((t (:inherit markdown-inline-code-face))))
+ `(org-block ((t (:background "lightgrey" :foreground "black" :extend t))))
+ `(org-block-begin-line ((t (:background unspecified :foreground "red3"))))
+ `(org-block-end-line ((t (:background unspecified :foreground "red3"))))
  ;; I like to keep my editor clean and simple. De-activate syntax highlighting
  ;; on some major programming related faces such as variables or functions, as
  ;; I don't think having lots of colors helps with focus and readability.
