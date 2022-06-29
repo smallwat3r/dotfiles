e396ad78614722a15a690ad237f1271c63878d92
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-06-29 15:19:28 +0100

    Switch zsh highlighting plugin

--- zsh/.zsh/extensions/10-antigen-plugins.zsh
+++ zsh/.zsh/extensions/10-antigen-plugins.zsh
@@ -3,12 +3,7 @@ if [ -f /usr/local/share/antigen/antigen.zsh ]; then
 
   antigen bundle zsh-users/zsh-autosuggestions
   antigen bundle hlissner/zsh-autopair
-  antigen bundle zdharma-continuum/fast-syntax-highlighting
+  antigen bundle zsh-users/zsh-syntax-highlighting
 
   antigen apply
-fi
-
-# TODO: fix this as this doesn't seem to work out of the box
-if [ ! -d "${FAST_WORK_DIR}" ]; then
-  fast-theme --quiet sv-orple
 fi
