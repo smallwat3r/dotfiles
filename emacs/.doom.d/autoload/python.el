1c67d7fae376b96cdc429b1665613e64aa247646
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-07-05 17:00:27 +0100

    Fix python venv settings

--- emacs/.doom.d/autoload/python.el
+++ emacs/.doom.d/autoload/python.el
@@ -19,6 +19,10 @@
 (defun my/deactivate-python-venv ()
   "Deactivate Python virtual environment."
   (interactive)
+  (setq lsp-pyright-venv-directory nil
+        lsp-pyright-venv-path nil
+        lsp-pyright-python-executable-cmd "python3"
+        python-shell-interpreter "python3")
   (pyvenv-deactivate)
   (message "Venv deactivated"))
 
@@ -27,7 +31,12 @@
   "Activate the closest Python virtual environment."
   (interactive)
   (if-let (venv-path-python (my/venv--locate-python-path))
-      (let ((venv-path venv-path-python))
+      (let* ((venv-path venv-path-python)
+             (python-executable (concat venv-path "/bin/python")))
+        (setq lsp-pyright-venv-directory my-python-virtual-env-directory
+              lsp-pyright-venv-path venv-path
+              lsp-pyright-python-executable-cmd python-executable
+              python-shell-interpreter python-executable)
         (pyvenv-activate venv-path)
         (message "Activated venv `%s'" venv-path))
     (message "Couldn't find any available venv")))
