e65d47a38d7f6cb92a1927042624b70843c7c0ae
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-07-12 17:56:15 +0100

    Small updates

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -182,7 +182,7 @@
   :commands (goto-address-prog-mode goto-address-mode)
   :config
   ;; Improve email regex matching, check for domain extension.
-  (setq goto-address-mail-regexp "\\w+\\(\\.\\w+\\)?@\\(\\w\\|\\.\\)+\\.\\w+")
+  (setq goto-address-mail-regexp "\\w+\\(\\.\\w+\\)?\\(\\+\\w+\\)?@\\(\\w\\|\\.\\)+\\.\\w+")
   ;; The default face for mail is `italic', which I don't like.
   (defface my-goto-address-mail-face
     '((t :background unspecified :inherit default :underline t))
