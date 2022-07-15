815361498844f950ed193ab39111939bf23fa4f4
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-07-15 16:39:57 +0100

    Bundle bindings in config.el

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -70,8 +70,6 @@
 ;;
 ;;; Editor
 
-(load! "+bindings")
-
 ;; Fonts
 (let* ((font "Monaco")
        (font-size 13))
@@ -635,8 +633,14 @@
 (use-package! bitwarden
   :custom
   (bitwarden-user user-mail-address)
-  (bitwarden-automatic-unlock
-   (lambda () (auth-source-pass-get 'secret "bitwarden/password"))))
+  (bitwarden-automatic-unlock (lambda ()
+                                (auth-source-pass-get 'secret "bitwarden/password")))
+  :init
+  (map! (:leader (:prefix "P"
+                  :desc "Bitwarden login"    "l" #'bitwarden-login
+                  :desc "Bitwarden unlock"   "u" #'bitwarden-unlock
+                  :desc "Bitwarden lock"     "L" #'bitwarden-lock
+                  :desc "Bitwarden list all" "b" #'bitwarden-list-all))))
 
 
 ;;
@@ -657,3 +661,95 @@
 (add-hook! 'post-command-hook
   (if (bound-and-true-p my-debug-mode)
       (my-echo-command-name-hook)))
+
+;;
+;;; Bindings
+
+;; Bundle most of my custom bindings for using Doom Emacs. Some bindings are not
+;; included here, when they are specific to packages and cannot be bundled outside
+;; of the package configuration.
+;;
+;; It's important for these to be loaded in last so it makes sure these are not
+;; getting overriden.
+
+;; Disable bindings, due to other conflicts (specially coming from Hammerspoon).
+(map! "M-k" nil
+      "M-j" nil)
+
+(map!
+ ;; For some reason, Emacs wouldn't print # on ^3, even when configured with
+ ;; an English UK keyboard. Re-enable this behaviour.
+ (:map key-translation-map
+  "M-3" "#")
+
+ (:map evil-insert-state-map
+  "C-h" #'evil-backward-char
+  "C-l" #'evil-forward-char
+  "C-k" #'evil-previous-line
+  "C-j" #'evil-next-line)
+
+ (:map evil-visual-state-map
+  ";f"  #'+format/region)
+
+ (:map evil-normal-state-map
+  "C-2"   #'my/scroll-up
+  "C-1"   #'my/scroll-down
+  "S-C-h" #'my/enlarge-window-horizontally
+  "S-C-l" #'my/shrink-window-horizontally
+  "S-C-k" #'my/enlarge-window
+  "S-C-j" #'my/shrink-window
+  "M-SPC" #'cycle-spacing
+  "M-o"   #'delete-blank-lines
+  ";d"    #'my/save-and-close-buffer
+  ";w"    #'my/save-buffer
+  "C-k"   #'join-line
+  "B"     #'beginning-of-line-text
+  "E"     #'end-of-line)
+
+ (:after python
+  (:map python-mode-map
+   (:leader
+    (:localleader
+     :desc "Open Python repl" "r" #'my/open-python-repl
+     (:prefix "i"
+      :desc "Remove unused imports" "R" #'pyimport-remove-unused)
+     (:prefix ("e" . "env")
+      :desc "Deactivate venv" "d" #'my/deactivate-python-venv
+      :desc "Activate venv"   "a" #'my/activate-closest-python-venv)))))
+
+ (:after vterm
+  (:map vterm-mode-map
+   :n "B"          #'vterm-beginning-of-line
+   :n "<return>"   #'evil-insert-resume
+   "<C-backspace>" #'my/vterm-delete-word
+   :in "C-k"       #'vterm-send-up
+   :in "C-j"       #'vterm-send-down))
+
+ (:leader
+  "§" #'other-frame
+  "1" #'my/where-am-i
+
+  (:prefix "b"
+   :desc "Kill buffer" "d" #'my/kill-buffer)
+
+  (:prefix "f"
+   :desc "Find file in dotfiles" "." #'my/find-file-in-dotfiles)
+
+  (:prefix "o"
+   :desc "Alacritty"              "a" #'my/alacritty-here
+   :desc "Link at point"          "l" #'browse-url-at-point
+   :desc "Vterm at root"          "T" #'+vterm/here
+   :desc "Toggle vterm at root"   "t" #'+vterm/toggle
+   :desc "Vterm at buffer"        "V" #'my/vterm/here-current-buffer
+   :desc "Toggle vterm at buffer" "v" #'my/vterm/toggle-current-buffer
+   :desc "Scr. buffer cur. mode"  "x" #'scratch
+   :desc "Scr. buffer restclient" "h" #'my/scratch-rest-mode)
+
+  (:prefix "t"
+   :desc "Truncate lines" "t" #'toggle-truncate-lines)
+
+  (:prefix "p"
+   :desc "Run Makefile target" "m" #'+make/run)
+
+  (:prefix ("P" . "password")
+   :desc "Open password-store buffer" "p" #'pass)))
