a1e2b3fb6601fbd4dc105001449a76b4937148b1
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-07-21 18:01:34 +0100

    Update qutebrowser config

--- qutebrowser/.qutebrowser/config.py
+++ qutebrowser/.qutebrowser/config.py
@@ -1,8 +1,8 @@
 config.load_autoconfig(False)
 
 # Fonts
-c.fonts.default_family = 'Monaco'
-c.fonts.default_size = '13pt'
+c.fonts.default_family = "Monaco"
+c.fonts.default_size = "13pt"
 
 # Editor
 c.editor.command = ["/usr/local/bin/emacsclient", "-c", "{}"]
@@ -11,4 +11,7 @@ c.editor.command = ["/usr/local/bin/emacsclient", "-c", "{}"]
 c.downloads.location.directory = "~/Downloads"
 
 # Stylesheets
-c.content.user_stylesheets = ['~/.qutebrowser/css/stylesheet.css']
+c.content.user_stylesheets = ["~/.qutebrowser/css/stylesheet.css"]
+
+# Allow javascript to access clipboard
+c.content.javascript.can_access_clipboard = True
