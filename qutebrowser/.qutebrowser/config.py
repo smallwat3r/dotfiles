00c1d5156db122bf6b0189eb01e18bf276fe407b
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-07-21 23:12:49 +0100

    Qutebrowser settings

--- qutebrowser/.qutebrowser/config.py
+++ qutebrowser/.qutebrowser/config.py
@@ -1,3 +1,4 @@
+# Do not load configs set via the UI
 config.load_autoconfig(False)
 
 # Fonts
@@ -15,3 +16,12 @@ c.content.user_stylesheets = ["~/.qutebrowser/css/stylesheet.css"]
 
 # Allow javascript to access clipboard
 c.content.javascript.can_access_clipboard = True
+
+# Default encoding
+c.content.default_encoding = "utf-8"
+
+# Custom headers
+c.content.headers.custom = {}
+
+# Enable smooth scrolling
+c.scrolling.smooth = True
