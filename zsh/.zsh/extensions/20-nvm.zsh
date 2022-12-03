12dbf2a55e956cce9286c64d0531fb061f162451
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2022-12-03 21:52:49 +0000

    Edit NVM settings

--- zsh/.zsh/extensions/20-nvm.zsh
+++ zsh/.zsh/extensions/20-nvm.zsh
@@ -1,11 +1,12 @@
 export NVM_DIR="${HOME}/.nvm"
 
-# I find nvm quite bulky to load on startup, so this provides a way to lazy load it.
+# nvm is quite bulky to load on startup, this provides a way to lazy load it.
 nvm() {
   if ! command -v nvm --help >/dev/null; then
-    if [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
-      source "/usr/local/opt/nvm/nvm.sh"
+    if [ -s "${HOME}/.nvm/nvm.sh" ]; then
+      source "${HOME}/.nvm/nvm.sh"
     fi
+
     if [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ]; then
       source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
     fi
