013d232088c0bd57d4d1089edba90942ed106a59
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-06-10 23:51:50 +0100

    Better configs

--- cross-platform/.config/qutebrowser/config.py
+++ cross-platform/.config/qutebrowser/config.py
@@ -5,12 +5,12 @@ config.load_autoconfig(False)
 
 if platform.system() == "Linux":
     c.fonts.default_family = "UW Ttyp0"
-    c.fonts.default_size = "14pt"
     c.editor.command = ["emacs", "{}"]
 else:
     c.fonts.default_family = "Triplicate A Code"
-    c.fonts.default_size = "14pt"
     c.editor.command = ["/usr/bin/emacsclient", "-c", "{}"]
+
+c.fonts.default_size = "14pt"
 
 # Download location
 c.downloads.location.directory = "~/Downloads"
@@ -27,11 +27,16 @@ c.content.default_encoding = "utf-8"
 # Custom headers
 c.content.headers.custom = {}
 
-# Bindings for vim like text editing
+# Bindings
 config.bind("$", "fake-key <End>")
 config.bind("0", "fake-key <Home>")
+
 config.bind("I", "fake-key <Home>;; mode-enter insert")
 config.bind("A", "fake-key <End>;; mode-enter insert")
+
 config.bind("cw", "fake-key <Alt-End><Backspace>;; mode-enter insert")
 config.bind("cc", "fake-key <Home><Shift-End><Delete>;; mode-enter insert")
 config.bind("<Shift-c>", "fake-key <Shift-End><Delete>;; mode-enter insert")
+
+config.bind("<Ctrl-j>", "completion-item-focus next", mode="command")
+config.bind("<Ctrl-k>", "completion-item-focus prev", mode="command")
