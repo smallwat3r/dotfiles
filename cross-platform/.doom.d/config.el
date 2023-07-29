e4367f13c3df1564cd5b49bae7c626b651e072e1
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-07-29 13:18:32 +0100

    Add comments

--- cross-platform/.doom.d/config.el
+++ cross-platform/.doom.d/config.el
@@ -998,7 +998,10 @@
    :desc "Open password-store buffer" "p" #'pass)
 
   (:prefix "s"
+   ;; Search a symbol at point using Vertico
    :desc "Search project (at point)" "w" #'my/vertico-search-project-symbol-at-point
+   ;; Repeat the last Vertico search. Doom also allow this with <SPC '> but I find it
+   ;; easier to remember memo-technically with <SPC s .>
    :desc "Repeat last search" "." #'vertico-repeat)))
 
 
