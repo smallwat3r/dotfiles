55bdfa1281fc964a8f87b50b19ffe726ef3609d3
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-08-21 18:31:50 +0100

    Docker-tramp is not needed in emacs > 29

--- cross-platform/.doom.d/modules/smallwat3r/containerization/packages.el
+++ cross-platform/.doom.d/modules/smallwat3r/containerization/packages.el
@@ -3,8 +3,10 @@
 
 ;; https://github.com/Silex/docker.el/commits/master
 (package! docker :pin "cc0046e6a557dce0ccc4108dd22e04f21ba8b0dc")
-;; https://github.com/emacs-pe/docker-tramp.el/commits/master
-(package! docker-tramp :pin "930d7b46c180d8a13240a028c1b40af84f2a3219")
+;; tramp-container (included with Emacs 29+) replaces docker-tramp
+(when (< emacs-major-version 29)
+  ;; https://github.com/emacs-pe/docker-tramp.el/commits/master
+  (package! docker-tramp :pin "930d7b46c180d8a13240a028c1b40af84f2a3219"))
 ;; https://github.com/spotify/dockerfile-mode/commits/master
 (package! dockerfile-mode :pin "52c6c00da1d31c0b6c29c74335b3af63ed6bf06c")
 ;; https://github.com/kubernetes-el/kubernetes-el/commits/master
