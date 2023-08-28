c31ecafdd6611bd916a51e355c04bee5f88583c3
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-08-28 15:17:55 +0100

    Small refactor

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -1,26 +1,26 @@
 -- smallwat3r"s hammerspoon config
 
-local hotkey = require "hs.hotkey"
-local grid = require "hs.grid"
-local hints = require "hs.hints"
-local window = require "hs.window"
-local alert = require "hs.alert"
-local task = require "hs.task"
-local eventtap = require "hs.eventtap"
-local chooser = require "hs.chooser"
+local hotkey = require 'hs.hotkey'
+local grid = require 'hs.grid'
+local hints = require 'hs.hints'
+local window = require 'hs.window'
+local alert = require 'hs.alert'
+local task = require 'hs.task'
+local eventtap = require 'hs.eventtap'
+local chooser = require 'hs.chooser'
 
-mod_alt = {"alt"}
-mod_cmd = {"cmd"}
-mod_ctrl = {"ctrl"}
-mod_ctrl_cmd = {"ctrl", "cmd"}
-mod_ctrl_alt = {"ctrl", "alt"}
+mod_alt = {'alt'}
+mod_cmd = {'cmd'}
+mod_ctrl = {'ctrl'}
+mod_ctrl_cmd = {'ctrl', 'cmd'}
+mod_ctrl_alt = {'ctrl', 'alt'}
 
 alert.defaultStyle = {
     strokeWidth  = 5,
     strokeColor = { white = 1, alpha = 1 },
     fillColor = { white = 0, alpha = 1 },
     textColor = { white = 1, alpha = 1 },
-    textFont = "Triplicate B",
+    textFont = 'Triplicate B',
     textSize = 16,
     radius = 0,
     atScreenEdge = 0,
@@ -34,18 +34,18 @@ alert.defaultStyle = {
 
 -- Spawn a new emacs client
 local function newEmacsClient()
-  alert.show("New Emacs client!")
-  task.new("/bin/zsh", nil,
-           { "-l", "-c", "emacsclient -a '' -c" }):start()
+  alert.show('New Emacs client!')
+  task.new('/bin/zsh', nil,
+           { '-l', '-c', 'emacsclient -a "" -c' }):start()
 end
 
-hotkey.bind(mod_cmd, "e", function() newEmacsClient() end)
+hotkey.bind(mod_cmd, 'e', function() newEmacsClient() end)
 
 -- Spawn an instance of emacs-everywhere
 local function emacsEverywhere()
-  alert.show("Emacs everywhere!")
-  task.new("/bin/zsh", nil,
-           { "-l", "-c", 'emacsclient -a "" --eval "(emacs-everywhere)"' }):start()
+  alert.show('Emacs everywhere!')
+  task.new('/bin/zsh', nil,
+           { '-l', '-c', 'emacsclient -a "" --eval "(emacs-everywhere)"' }):start()
 end
 
 -- TODO: remap this, as I keep hitting it by mistake, also should I keep using this?
@@ -56,21 +56,21 @@ local function confirmationDialog(actionFunc)
   test = chooser.new(actionFunc)
   test:rows(2)
   test:choices({
-    {["text"] = "Yes", ["id"] = "yes", ["subText"] = "Kill the running Emacs daemon"},
-    {["text"] = "No", ["id"] = "no", ["subText"] = "Leave the Emacs daemon running"}
+    {['text'] = 'Yes', ['id'] = 'yes', ['subText'] = 'Kill the running Emacs daemon'},
+    {['text'] = 'No', ['id'] = 'no', ['subText'] = 'Leave the Emacs daemon running'}
   })
   test:show()
 end
 
 local function stopEmacsDaemon(input)
-  if input and input.id == "yes" then
-    task.new("/bin/zsh", nil,
-             { "-l", "-c", 'emacsclient -e "(kill-emacs)"' }):start()
-    alert.show("Stopped Emacs daemon!")
+  if input and input.id == 'yes' then
+    task.new('/bin/zsh', nil,
+             { '-l', '-c', 'emacsclient -e "(kill-emacs)"' }):start()
+    alert.show('Stopped Emacs daemon!')
   end
 end
 
-hotkey.bind(mod_cmd, "`", function() confirmationDialog(stopEmacsDaemon) end)
+hotkey.bind(mod_cmd, '`', function() confirmationDialog(stopEmacsDaemon) end)
 
 -- Window management
 -- ***************************************************************************
@@ -83,26 +83,26 @@ grid.GRIDHEIGHT = 4
 grid.GRIDWIDTH = 4
 
 -- Center window
-hotkey.bind(mod_cmd, "[", function() window.focusedWindow():centerOnScreen() end)
+hotkey.bind(mod_cmd, '[', function() window.focusedWindow():centerOnScreen() end)
 
 -- Fullscreen window
-hotkey.bind(mod_cmd, "]", function() window.focusedWindow():maximize(0) end)
+hotkey.bind(mod_cmd, ']', function() window.focusedWindow():maximize(0) end)
 
 -- Move window
-hotkey.bind(mod_ctrl_alt, "j", grid.pushWindowDown)
-hotkey.bind(mod_ctrl_alt, "k", grid.pushWindowUp)
-hotkey.bind(mod_ctrl_alt, "h", grid.pushWindowLeft)
-hotkey.bind(mod_ctrl_alt, "l", grid.pushWindowRight)
+hotkey.bind(mod_ctrl_alt, 'j', grid.pushWindowDown)
+hotkey.bind(mod_ctrl_alt, 'k', grid.pushWindowUp)
+hotkey.bind(mod_ctrl_alt, 'h', grid.pushWindowLeft)
+hotkey.bind(mod_ctrl_alt, 'l', grid.pushWindowRight)
 
 -- Resize window
-hotkey.bind(mod_alt, "k", grid.resizeWindowShorter)
-hotkey.bind(mod_alt, "j", grid.resizeWindowTaller)
-hotkey.bind(mod_alt, "l", grid.resizeWindowWider)
-hotkey.bind(mod_alt, "h", grid.resizeWindowThinner)
+hotkey.bind(mod_alt, 'k', grid.resizeWindowShorter)
+hotkey.bind(mod_alt, 'j', grid.resizeWindowTaller)
+hotkey.bind(mod_alt, 'l', grid.resizeWindowWider)
+hotkey.bind(mod_alt, 'h', grid.resizeWindowThinner)
 
 -- Show window hints
-hints.style = "vimperator"
-hotkey.bind(mod_alt, "Tab", function() hints.windowHints() end)
+hints.style = 'vimperator'
+hotkey.bind(mod_alt, 'Tab', function() hints.windowHints() end)
 
 -- Make the combination of Cmd + hjkl to emulate the arrow keys behaviour.
 -- ***************************************************************************
@@ -122,42 +122,49 @@ local function remap(mods, key, pressFn)
   hotkey.bind(mods, key, pressFn, nil, pressFn)
 end
 
-remap(mod_cmd, "h", pressFn("left"))
-remap(mod_cmd, "j", pressFn("down"))
-remap(mod_cmd, "k", pressFn("up"))
-remap(mod_cmd, "l", pressFn("right"))
+remap(mod_cmd, 'h', pressFn('left'))
+remap(mod_cmd, 'j', pressFn('down'))
+remap(mod_cmd, 'k', pressFn('up'))
+remap(mod_cmd, 'l', pressFn('right'))
 
 -- Custom launchers
 -- ***************************************************************************
 
 -- check if a window with name already exists, and focus it, or run task
-local function launcherRunner(windowName, scriptLauncher)
+local function launcherRunner(windowName, scriptLauncher, alacrittyOptions)
   local window = window.get(windowName)
   if window then
     window:focus()
   else
-    local command = 'INSIDE_HS=1 alacritty -T "%s" --config-file $HOME/.config/launcher.yml -e %s'
-    task.new("/bin/zsh", nil, {"-l", "-c", string.format(command, windowName, scriptLauncher)}):start()
+    local command
+    if alacrittyOptions then
+      command = string.format('INSIDE_HS=1 alacritty -T "%s" --config-file $HOME/.config/launcher.yml -o %s -e %s',
+                              windowName, alacrittyOptions, scriptLauncher)
+    else
+      command = string.format('INSIDE_HS=1 alacritty -T "%s" --config-file $HOME/.config/launcher.yml -e %s',
+                              windowName, scriptLauncher)
+    end
+    task.new('/bin/zsh', nil, { '-l', '-c', command }):start()
   end
 end
 
 -- open an application
-hotkey.bind(mod_cmd, "m", function() launcherRunner('App launcher', 'launcher-app') end)
+hotkey.bind(mod_cmd, 'm', function() launcherRunner('App launcher', 'launcher-app', nil) end)
 
 -- execute a binary
-hotkey.bind(mod_cmd, ",", function() launcherRunner('Bin launcher', 'launcher-bin') end)
+hotkey.bind(mod_cmd, ',', function() launcherRunner('Bin launcher', 'launcher-bin', nil) end)
 
 -- browse google chrome bookmarks
-hotkey.bind(mod_cmd, ".", function() launcherRunner('Bookmark launcher', 'launcher-chrome-bookmarks') end)
+hotkey.bind(mod_cmd, '.', function() launcherRunner('Bookmark launcher', 'launcher-chrome-bookmarks', nil) end)
 
 -- browse google chrome history
-hotkey.bind(mod_cmd, "/", function() launcherRunner('History launcher', 'launcher-chrome-history') end)
+hotkey.bind(mod_cmd, '/', function() launcherRunner('History launcher', 'launcher-chrome-history', 'window.dimensions.columns=100') end)
 
 -- switch to open application
-hotkey.bind(mod_cmd, "space", function() launcherRunner('App switcher', 'launcher-running-app') end)
+hotkey.bind(mod_cmd, 'space', function() launcherRunner('App switcher', 'launcher-running-app', nil) end)
 
 -- search files from home directory
-hotkey.bind(mod_cmd, ";", function() launcherRunner('File launcher', 'launcher-file-search') end)
+hotkey.bind(mod_cmd, ';', function() launcherRunner('File launcher', 'launcher-file-search', nil) end)
 
 -- perform a google search
-hotkey.bind(mod_cmd, "n", function() launcherRunner('Google search', 'chrome-search') end)
+hotkey.bind(mod_cmd, 'n', function() launcherRunner('Google search', 'chrome-search', 'window.dimensions.lines=3') end)
