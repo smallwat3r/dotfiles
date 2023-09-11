4a7033f59064c5c0bde99c3d85af9f3f7ca8a0f0
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-09-11 11:24:46 +0100

    Utility function to play with nearest python venv

--- cross-platform/.zsh/extensions/20-python.zsh
+++ cross-platform/.zsh/extensions/20-python.zsh
@@ -10,3 +10,25 @@ fi
 if [ -d "/Library/Frameworks/Python.framework/Versions/3.10/bin" ]; then
   export PATH="/Library/Frameworks/Python.framework/Versions/3.10/bin:${PATH}"
 fi
+
+# activate the nearest python venv
+activate_nearest_venv() {
+  local dir
+  dir="$(pwd)"
+  while [[ ! -f "${dir}/venv/bin/activate" && -n "${dir}" ]]; do
+    dir="${dir%/*}"
+  done
+  if [[ -f "${dir}/venv/bin/activate" ]]; then
+    echo "Activating virtualenv from ${dir}/venv"
+    # shellcheck disable=SC1091
+    source "${dir}/venv/bin/activate"
+  else
+    echo 'No venv found'
+    false
+  fi
+}
+
+# run python from the nearest venv
+vpython() {
+  activate_nearest_venv && python "$@"
+}
