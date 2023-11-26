7067859f28208cb3f5ad15ba1dd2eecf2af062fd
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-11-26 18:00:05 +0000

    Remove usage of Antigen

---
+++ cross-platform/.zsh/extensions/10-plugins.zsh
@@ -0,0 +1,6 @@
+if [ -f '/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh' ]; then
+  source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+if [ -f '/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ]; then
+ source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
