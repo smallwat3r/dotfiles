a67cc7e2ee20b69dfeca31ce909aa60720b0ca09
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-11-29 13:33:32 +0000

    Add indicator for git pause

--- cross-platform/.zsh/core/10-prompt.zsh
+++ cross-platform/.zsh/core/10-prompt.zsh
@@ -7,6 +7,42 @@ __is_venv() {
   fi
 }
 
+# Return true if the current directory is the root of a git repository.
+__git_root() {
+  if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]]; then
+    [[ $(git rev-parse --show-toplevel 2>/dev/null) == "${PWD}" ]] && echo true
+  fi
+}
+
+# Show a `*` next to the branch name if the Git branch is dirty.
+__git_dirty() {
+  [[ $(git diff --shortstat 2>/dev/null | tail -n1) != "" ]] && echo "*"
+}
+
+# Red indicator that the current branch has been PAUSED with git-pause, need
+# to use git-resume to resume it.
+__git_is_paused() {
+  [[ $(git log -1 --format="%s" 2>/dev/null | grep '^PAUSED') ]] &&
+    echo "%B%F{198}%K{52}[${(U)}PAUSED]%b%f%k "
+}
+
+# Display current git branch.
+__git_branch() {
+  git branch --no-color 2>/dev/null \
+    | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(__git_dirty)/"
+}
+
+# Display git information.
+__display_git_info() {
+  [[ ! $(git rev-parse --is-inside-work-tree 2>/dev/null) ]] && return
+
+  local _git_root="%F{yellow}$(__git_root | sed 's/true/~/')%f"
+  local _git_branch="%F{yellow}$(__git_branch)%f"
+  local _git_is_paused="$(__git_is_paused)"
+
+  echo " ${_git_is_paused}${_git_branch}${_git_root}"
+}
+
 # Disable showing any Python virtual environment information in the shell prompt.
 # Indeed I'm using my own function to display this information.
 VIRTUAL_ENV_DISABLE_PROMPT=false
@@ -26,9 +62,6 @@ precmd_vcs_info() {
 precmd_functions+=(
   precmd_vcs_info
 )
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' check-for-changes false  # less expensive
-zstyle ':vcs_info:*' formats ' (%F{yellow}%b%f)'
 
 # Manually input custom text in the prompt. Fetch the value of the tag from the
 # `_PROMPT_TAG` variable and add custom colors and faces.
@@ -46,7 +79,7 @@ tag() {
 
 # Prompt format definition. It will print out return codes in red in case the
 # command fails.
-PROMPT='%(?..%F{red}?%? )$(__tag)$(__is_venv)%f%3~%f${vcs_info_msg_0_}%# '
+PROMPT='%(?..%F{red}?%? )$(__tag)$(__is_venv)%f%3~%f$(__display_git_info) %# '
 
 # When outside of emacs, activate tmux by default and use the individual pane
 # titles to display the main prompt information.
@@ -69,31 +102,6 @@ if ((! ${+INSIDE_EMACS} )) && ((! ${+INSIDE_HS})); then
         -t "$(__pane_number)" \
         -T "#[fg=red,bold]$(echo "$*" | cut -d . -f 1)#[fg=default]"
     command ssh "${@}"
-  }
-
-  # Return true if the current directory is the root of a git repository.
-  __git_root() {
-    if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]]; then
-      [[ $(git rev-parse --show-toplevel 2>/dev/null) == "${PWD}" ]] && echo true
-    fi
-  }
-
-  # Show a `*` next to the branch name if the Git branch is dirty.
-  __git_dirty() {
-    [[ $(git diff --shortstat 2>/dev/null | tail -n1) != "" ]] && echo "*"
-  }
-
-  # Display current git branch.
-  __git_branch() {
-    git branch --no-color 2>/dev/null \
-      | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(__git_dirty)/"
-  }
-
-  # Display git information.
-  __display_git_info() {
-    local _git_root="$(__git_root | sed 's/true/~/')"
-    local _git_branch="$(__git_branch)"
-    [[ -n ${_git_branch} ]] && echo " ${_git_branch}${_git_root}"
   }
 
   # Shrink the current directory path.
