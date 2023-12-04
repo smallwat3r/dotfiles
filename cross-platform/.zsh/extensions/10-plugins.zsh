8d43c8e7817b906216d7a1fa28713961cc2c8dca
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-12-04 21:40:28 +0000

    Source ZSH plugins

--- cross-platform/.zsh/extensions/10-plugins.zsh
+++ cross-platform/.zsh/extensions/10-plugins.zsh
@@ -1,6 +1,18 @@
+# macos
 if [ -f '/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh' ]; then
   source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 fi
 if [ -f '/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ]; then
  source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+# linux
+if [ -f '/usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh ' ]; then
+  source /usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+fi
+if [ -f '/usr/share/zsh/plugins/zsh-autosuggestions//zsh-autosuggestions.plugin.zsh' ]; then
+  source /usr/share/zsh/plugins/zsh-autosuggestions//zsh-autosuggestions.plugin.zsh
+fi
+if [ -f '/usr/share/zsh/plugins/zsh-syntax-highlighting//zsh-syntax-highlighting.plugin.zsh' ]; then
+  source /usr/share/zsh/plugins/zsh-syntax-highlighting//zsh-syntax-highlighting.plugin.zsh
 fi
