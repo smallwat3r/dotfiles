1f27177479dbaf4255f52b28e82e97258c98cc8a
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-12-09 13:33:35 +0000

    Only loop on necessary files

--- cross-platform/.zsh/extensions/10-plugins.zsh
+++ cross-platform/.zsh/extensions/10-plugins.zsh
@@ -1,14 +1,16 @@
 __load_plugins() {
-  local plugins=(
-    # macos
-    '/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh'
-    '/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
-    # linux
-    '/usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh'
-    '/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh'
-    '/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh'
-  )
-
+  if [[ $(uname) == 'Darwin' ]]; then
+    local plugins=(
+      '/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh'
+      '/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+    )
+  else
+    local plugins=(
+      '/usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh'
+      '/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh'
+      '/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh'
+    )
+  fi
   local plugin
   for plugin ("${plugins[@]}"); do
     if [ -f "${plugin}" ]; then
