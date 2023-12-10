f26795289f8dd5fa7af9f4a3398881e3656012e3
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-12-10 22:18:46 +0000

    Easier history search

--- cross-platform/.zsh/core/40-bind-history-search.zsh
+++ cross-platform/.zsh/core/40-bind-history-search.zsh
@@ -1,9 +1,6 @@
 # Provides bindings to go through commands history
 
-autoload -U history-search-end
+autoload -U history-search
 
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-
-bindkey "^[[A" history-beginning-search-backward-end
-bindkey "^[[B" history-beginning-search-forward-end
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
