cad7f8e36bf4122bc3962d1ed771b09ba0a15cb1
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-03-05 17:07:51 +0000

    Update fonts

--- cross-platform/.config/qutebrowser/config.py
+++ cross-platform/.config/qutebrowser/config.py
@@ -4,11 +4,11 @@ import platform
 config.load_autoconfig(False)
 
 if platform.system() == "Linux":
-    c.fonts.default_family = "Maple Mono NF"
+    c.fonts.default_family = "Vulf Mono Code"
     c.editor.command = ["emacs", "{}"]
     c.fonts.default_size = "11pt"
 else:
-    c.fonts.default_family = "Maple Mono NF"
+    c.fonts.default_family = "Vulf Mono Code"
     c.editor.command = ["/usr/bin/emacsclient", "-c", "{}"]
     c.fonts.default_size = "12pt"
 
