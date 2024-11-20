11de4560c7432580a7470938df7f684e0b4e245d
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-11-20 22:46:08 +0000

    Change main coding font to Intel Mono

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -19,7 +19,7 @@ alert.defaultStyle = {
     strokeColor = { white = 1, alpha = 1 },
     fillColor = { white = 0, alpha = 1 },
     textColor = { white = 1, alpha = 1 },
-    textFont = 'Hurmit Nerd Font Mono',
+    textFont = 'Intel One Mono',
     textSize = 16,
     radius = 0,
     atScreenEdge = 0,
