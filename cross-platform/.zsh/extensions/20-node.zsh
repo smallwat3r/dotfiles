e3d57b5ef0b5802572db9cf4a5da3e030a8c5a7c
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-02-19 12:15:00 +0000

    Lazy load nvm

--- cross-platform/.zsh/extensions/20-node.zsh
+++ cross-platform/.zsh/extensions/20-node.zsh
@@ -1,1 +1,7 @@
 export PATH="/opt/homebrew/opt/node@22/bin:$PATH"
+
+load_nvm() {
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+}
