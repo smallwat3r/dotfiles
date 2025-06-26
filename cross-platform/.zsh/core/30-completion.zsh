77c42b74cb4fec702c3f98ba2cfe583a20b53e44
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-06-26 16:25:32 +0100

    Add back ZSH completion

--- cross-platform/.zsh/core/30-completion.zsh
+++ cross-platform/.zsh/core/30-completion.zsh
@@ -1,36 +1,39 @@
 # ZSH completion configuration
 
-# Don't enable completion by default, as I realised I don't use this a lot.
-# Still, provide a way to enable it on demand from calling `zsh_enable_completion`.
+autoload -U compinit && compinit
 
-zsh_enable_completion() {
-  autoload -U compinit && compinit
+setopt COMPLETE_IN_WORD  # complete from both end of a word
+setopt AUTO_MENU         # use menu completion after the second consecutive request for completion
+setopt AUTO_LIST         # list choices on an ambiguous completion
+setopt AUTO_PARAM_SLASH  # add a trailing slash instead of a space in case of a directory
+setopt ALWAYS_TO_END     # move cursor to the end after completion
 
-  setopt COMPLETE_IN_WORD  # complete from both end of a word
-  setopt AUTO_MENU         # use menu completion after the second consecutive request for completion
-  setopt AUTO_LIST         # list choices on an ambiguous completion
-  setopt AUTO_PARAM_SLASH  # add a trailing slash instead of a space in case of a directory
-  setopt ALWAYS_TO_END     # move cursor to the end after completion
+# enable caching for faster completion
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "${HOME}/.cache/.zcompcache"
 
-  zstyle ':completion:*' use-cache on
-  zstyle ':completion:*' cache-path "${HOME}/.cache/.zcompcache"
-  zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  zstyle ':completion:*' menu select
-  zstyle ':completion:*' squeeze-slashes true
-  zstyle ':completion:*' verbose yes
-  zstyle ':completion:*' file-sort modification
-  zstyle ':completion:*' completer _complete _list _match _approximate
-  zstyle ':completion:*:matches' group 'yes'
-  zstyle ':completion:*:options' description 'yes'
-  zstyle ':completion:*:options' auto-description '%d'
-  zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
-  zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
-  zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-  zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-  zstyle ':completion:*' group-name ''
+# completion matching behavior
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' \
+       'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-  # Expand aliases with C-a
-  zle -C alias-expension complete-word _generic
-  bindkey '^a' alias-expension
-  zstyle ':completion:alias-expension:*' completer _expand_alias
-}
+# interface enhancements
+zstyle ':completion:*' menu select
+zstyle ':completion:*' squeeze-slashes true
+zstyle ':completion:*' verbose no
+zstyle ':completion:*' file-sort modification
+zstyle ':completion:*' completer _complete _list _match _approximate
+zstyle ':completion:*:corrections' format '%F{green}-- %d (errors: %e)%f'
+zstyle ':completion:*:descriptions' format '%F{cyan}-- %d%f'
+zstyle ':completion:*:messages' format '%F{blue}-- %d%f'
+zstyle ':completion:*:warnings' format '%F{magenta}-- no matches found%f'
+
+# grouping and descriptions
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*' group-name ''
+
+# expand aliases with C-a
+zle -C alias-expension complete-word _generic
+bindkey '^a' alias-expension
+zstyle ':completion:alias-expension:*' completer _expand_alias
