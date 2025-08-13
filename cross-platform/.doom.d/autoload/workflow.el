75ffe03ca6abebd39525d0ec50a2bdb91ae795bf
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-08-13 16:27:59 +0100

    Add function to kill active process

--- cross-platform/.doom.d/autoload/workflow.el
+++ cross-platform/.doom.d/autoload/workflow.el
@@ -79,7 +79,6 @@
   (interactive)
   (+vertico/project-search arg (thing-at-point 'symbol)))
 
-
 ;;;###autoload
 (defun my/open-remote-conn ()
   "Open remote SSH connection with Tramp."
@@ -87,3 +86,24 @@
   (let ((tramp-path "/ssh:")
         (prompt "Pick target: "))
     (find-file (read-file-name prompt tramp-path))))
+
+;;;###autoload
+(defun my/fuzzy-kill-process ()
+  "Fuzzy-pick a system process and send SIGKILL."
+  (interactive)
+  (let* ((fmt-item
+          (lambda (pid)
+            (let ((a (process-attributes pid)))
+              (when a
+                (let* ((name (alist-get 'comm a))
+                       (pcpu (or (alist-get 'pcpu a) 0.0))
+                       (rss  (alist-get 'rss a))
+                       (rss-mb (if (numberp rss) (/ rss 1024.0) 0.0)))
+                  (cons (format "%-20s %6d  %7.1fMB  %5.1f%%"
+                                (or name "?") pid rss-mb pcpu)
+                        pid))))))
+         (items (delq nil (mapcar fmt-item (list-system-processes))))
+         (choice (completing-read "Kill process: " (mapcar #'car items) nil t))
+         (pid (cdr (assoc choice items))))
+    (signal-process pid 'kill)
+    (message "Killed %s" choice)))
