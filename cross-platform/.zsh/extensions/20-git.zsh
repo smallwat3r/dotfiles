95c305b64d31c49b7da1737231aa20c415aeed7d
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-10-21 15:40:48 +0100

    Add git SSH command

---
+++ cross-platform/.zsh/extensions/20-git.zsh
@@ -0,0 +1,9 @@
+# Git related configurations
+
+export GIT_SSH_COMMAND='ssh -4 \
+  -o ConnectTimeout=10 \
+  -o ServerAliveInterval=20 \
+  -o ServerAliveCountMax=3 \
+  -o TCPKeepAlive=yes \
+  -o GSSAPIAuthentication=no \
+  -o ControlMaster=no'
