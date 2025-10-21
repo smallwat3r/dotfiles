bf7bed057e94b3a67c3ce0e735b1bcc01eca9491
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-10-21 17:54:10 +0100

    Ensure git ssh custom options are used within Emacs

--- cross-platform/.doom.d/config.el
+++ cross-platform/.doom.d/config.el
@@ -348,6 +348,15 @@
           "A" #'git-rebase-move-line-down ; custom layout
           )))
 
+;; all Git SSH commands from Emacs should use this
+(setenv "GIT_SSH_COMMAND" "ssh -4 \
+  -o ConnectTimeout=10 \
+  -o ServerAliveInterval=20 \
+  -o ServerAliveCountMax=3 \
+  -o TCPKeepAlive=yes \
+  -o GSSAPIAuthentication=no \
+  -o ControlMaster=no")
+
 ;; git-timemachine
 ;; doc: https://github.com/emacsmirror/git-timemachine
 (after! git-timemachine
