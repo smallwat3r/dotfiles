410aa45857c7ab59c4f0ac43eaa24970099c6f2c
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-11-18 23:28:47 +0000

    Better way to load plugins

--- cross-platform/.zsh/extensions/99-plugins.zsh
+++ cross-platform/.zsh/extensions/99-plugins.zsh
@@ -1,44 +1,39 @@
 # Manage any external tools / scripts, such as autosuggestions and syntax highlighting.
 
 __load_plugins() {
-  local plugins plugin
+  emulate -L zsh
 
-  case "$(uname)" in
-    Darwin)
-      plugins=(
-        '/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh'
-        '/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
-        '/opt/homebrew/share/zsh-history-substring-search/zsh-history-substring-search.zsh'
-      )
-      ;;
-    Linux)
-      # Fedora detection
-      if [[ -f /etc/os-release ]] && grep -qi '^id=fedora' /etc/os-release; then
-        plugins=(
-          '/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh'
-          '/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
-          '/usr/share/zsh-history-substring-search/zsh-history-substring-search.zsh'
-        )
-      else
-        # Generic Linux distros
-        plugins=(
-          '/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh'
-          '/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh'
-          '/usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh'
-        )
-      fi
-      ;;
-    *)
-      plugins=() # Unknown system
-      ;;
+  # plugin directory roots to probe per OS
+  local -a basedirs
+  case "$(uname -s)" in
+    Darwin) basedirs=(/opt/homebrew/share) ;;
+    Linux) basedirs=(/usr/share /usr/share/zsh) ;;
+    *) basedirs=() ;;
   esac
 
-  for plugin in "${plugins[@]}"; do
-    if [[ -f $plugin ]]; then
-      source "$plugin"
-    else
-      echo "Plugin not found: $plugin" >&2
-    fi
+  # plugin names, used to build paths
+  local -a plugins=(
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    zsh-history-substring-search
+  )
+
+  local plugin dir path loaded
+  for plugin in $plugins; do
+    loaded=0
+    for dir in $basedirs; do
+      for path in \
+        "$dir/$plugin/$plugin.zsh" \
+        "$dir/plugins/$plugin/$plugin.plugin.zsh"
+      do
+        if [[ -f $path ]]; then
+          source "$path"
+          loaded=1
+          break 2   # break out of both inner loops
+        fi
+      done
+    done
+    (( loaded )) || print -u2 "Plugin not found: $plugin"
   done
 }
 
