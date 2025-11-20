ee1da77d6d5b598dea071109f0f83f64c95b9cb9
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-11-20 23:38:23 +0000

    Bundle options in general configurations

--- cross-platform/.zsh/core/10-general.zsh
+++ cross-platform/.zsh/core/10-general.zsh
@@ -1,4 +1,22 @@
+# General configurations
+
+unsetopt BEEP               # do no beep on errors
+unsetopt LIST_BEEP          # do not beep on anbiguous completion
+unsetopt COMPLETE_ALIASES   # do not prevent aliases from being substituted before completion is attempted
+
+setopt AUTO_CD              # auto cd into typed directory
+setopt CHASE_LINKS          # resolve symlinks to their true values when changing directory
+setopt GLOB_DOTS            # do not require a leading '.' in a filename to be matched explicitly
+setopt INTERACTIVE_COMMENTS # allow comments in interactive shell
+setopt LIST_PACKED          # make the completion list occupying less lines
+
 # history configs
+setopt APPEND_HISTORY       # keep history of commands
+setopt EXTENDED_HISTORY     # add timestamp and duration to the history
+setopt INC_APPEND_HISTORY   # add commands as soon as they are entered
+setopt HIST_REDUCE_BLANKS   # get rid of superfluous blank lines
+setopt HIST_VERIFY          # perform history expansion and reload the line into the editing buffer
+setopt HISTIGNORESPACE      # do not save in history commands that starts by a space
 export HISTFILE="${HOME}/.zsh_history"
 export HISTSIZE=999999999
 export SAVEHIST="${HISTSIZE}"
