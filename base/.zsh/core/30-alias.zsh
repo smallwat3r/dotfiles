ba07a9fd41cb03f21245eb4bfbfb17db2e6cc8b0
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-01-28 21:32:13 +0000

    Fix

--- base/.zsh/core/30-alias.zsh
+++ base/.zsh/core/30-alias.zsh
@@ -1,42 +1,42 @@
-alias c="clear"
-alias p="pwd"
-alias qq="exit"
+alias c='clear'
+alias p='pwd'
+alias qq='exit'
 alias e="$EDITOR"
 
 # Handy to allow ignoring prompt characters when copying commands from
 # documentation. Also acts as an alias to make a command not appear in
 # the history when using HIST_IGNORE_SPACE (HIST_IGNORE_SPACE in zsh).
 alias \
-  \$=" " \
-  %=" "
+  \$=' ' \
+  %=' '
 
 # List files
 alias \
-  l="ls -pFf --color" \
-  ll="ls -lah --color" \
-  ls.="ls -pF -d .* --color" \
-  ls="ls -pF --color" \
-  sl="ls -pF --color" \
-  lss="ls -l *(@)"
+  l='ls -pFf --color' \
+  ll='ls -lah --color' \
+  ls.='ls -pF -d .* --color' \
+  ls='ls -pF --color' \
+  sl='ls -pF --color' \
+  lss='ls -l *(@)'
 
 # Use Neovim over Vim
 alias \
-  vim="nvim" \
-  v="nvim"
+  vim='nvim' \
+  v='nvim'
 
 # Ripgrep
-alias rg="rg \
+alias rg='rg \
   --max-columns 120 \
-  --glob '!.git/*' \
-  --glob '!__pycache__/*' \
+  --glob "!.git/*" \
+  --glob "!__pycache__/*" \
   --smart-case \
-  --colors 'line:fg:cyan' \
-  --colors 'path:fg:cyan' \
-  --colors 'match:fg:red' "
+  --colors "line:fg:cyan" \
+  --colors "path:fg:cyan" \
+  --colors "match:fg:red" '
 
 # Misc
-alias mkdir="mkdir -pv"
-alias diskspace="df -P -kHl"
+alias mkdir='mkdir -pv'
+alias diskspace='df -P -kHl'
 alias dots="cd $HOME/dotfiles"
 
 # macOS-only helpers
@@ -44,12 +44,12 @@ is_macos && alias fonts="open $HOME/Library/Fonts"
 
 # Global aliases
 alias \
-    -g G="| grep" \
-    -g L="| less" \
-    -g NE="2> /dev/null" \
-    -g H="| head" \
-    -g T="| tail" \
-    -g S="| sort"
+  -g G='| grep' \
+  -g L='| less' \
+  -g NE='2> /dev/null' \
+  -g H='| head' \
+  -g T='| tail' \
+  -g S='| sort'
 
 alias -g C="| $(_clip_cmd)"
 
@@ -63,7 +63,7 @@ hash -d \
   ssh="$HOME/.ssh" \
   zsh="$HOME/.zsh" \
   config="$HOME/.config" \
-  etc="/etc" \
-  opt="/opt"
+  etc='/etc' \
+  opt='/opt'
 
 is_macos && hash -d fonts="$HOME/Library/Fonts"
