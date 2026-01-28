ccef1cdf379ce8239c8081a55b08c9ac70bc8100
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-01-28 10:37:37 +0000

    Pin eat

--- base/.doom.d/modules/smallwat3r/terminal/packages.el
+++ base/.doom.d/modules/smallwat3r/terminal/packages.el
@@ -1,3 +1,6 @@
-;;; smallwat3r/terminal/packages.el -*- lexical-binding: t; -*-
+;; -*- no-byte-compile: t; -*-
+;;; smallwat3r/terminal/packages.el
 
-(package! eat)
+;; Emulate A Terminal, a terminal emulator for Emacs
+;; https://codeberg.org/akib/emacs-eat/commits/branch/master
+(package! eat :pin "c8d54d649872bfe7b2b9f49ae5c2addbf12d3b99")
