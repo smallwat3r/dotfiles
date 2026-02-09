dd8ec79285c8fb2510b46031f3d57d85a24ee6d8
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-02-09 23:30:18 +0000

    Remove unused feature

--- base/.zsh/core/10-general.zsh
+++ base/.zsh/core/10-general.zsh
@@ -1,8 +1,8 @@
 # General zsh configuration
 #
 # Shell options, history settings, and key bindings for a comfortable
-# interactive experience. Includes Ctrl+E to edit command line,
-# Ctrl+O to toggle quotes, and smart history search with arrow keys.
+# interactive experience. Includes Ctrl+E to edit command line and
+# smart history search with arrow keys.
 
 unsetopt BEEP               # do no beep on errors
 unsetopt LIST_BEEP          # do not beep on anbiguous completion
@@ -55,47 +55,3 @@ bindkey '^D' __exit_zsh
 bindkey "^[[A" history-beginning-search-backward
 bindkey "^[[B" history-beginning-search-forward
 
-# toggle quotes around a word
-__toggle_quotes() {
-  emulate -L zsh
-  setopt localoptions extendedglob
-
-  local l=$LBUFFER
-  local r=$RBUFFER
-
-  # find start of the word under cursor
-  local i=$CURSOR
-  while (( i > 0 )) && [[ ${l[i]} != " " && ${l[i]} != $'\t' ]]; do
-    (( i-- ))
-  done
-  local start=$(( i + 1 ))
-
-  # find end of the word under cursor
-  local len=${#BUFFER}
-  local j=$(( CURSOR + 1 ))
-  while (( j <= len )) && [[ ${BUFFER[j]} != " " && ${BUFFER[j]} != $'\t' ]]; do
-    (( j++ ))
-  done
-  local end=$(( j - 1 ))
-
-  # if no word found, do nothing
-  (( end < start )) && return
-
-  # extract word + prefix + suffix
-  local prefix=${BUFFER[1,start-1]}
-  local word=${BUFFER[start,end]}
-  local suffix=${BUFFER[end+1,-1]}
-
-  if [[ $word == \"*\" && $word == *\" ]]; then
-    # already quoted -> remove
-    local inner=${word[2,-2]}
-    BUFFER="${prefix}${inner}${suffix}"
-    CURSOR=$(( ${#prefix} + ${#inner} ))
-  else
-    # unquoted -> add quotes
-    BUFFER="${prefix}\"${word}\"${suffix}"
-    CURSOR=$(( ${#prefix} + ${#word} + 2 ))
-  fi
-}
-zle -N __toggle_quotes
-bindkey '^O' __toggle_quotes
