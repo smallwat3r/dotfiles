9d6ac0dc52b65e9da943fd0a5bf597c1ee2f232a
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-08-29 18:51:36 +0100

    Delete config

--- base/.zsh/tools/20-foxglove.zsh
+++
@@ -1,8 +0,0 @@
-# Foxglove Studio (snap)
-#
-# The snap sandbox only maps wayland-0, but the compositor uses wayland-1,
-# so native Wayland segfaults. Force XWayland instead.
-
-has foxglove-studio || return
-
-alias foxglove-studio='foxglove-studio --ozone-platform=x11'
